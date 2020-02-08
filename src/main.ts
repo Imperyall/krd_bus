460d00edc6a54c0c2adbe4f43832ee153d344be6
Roman <roma.kamaz@gmail.com>
2020-02-09 00:56:58 +0300

    fontawesome yandex maps

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import YmapPlugin from 'vue-yandex-maps'
 
 Vue.config.productionTip = false
+
+const YandexMapsSettings = {
+  apiKey: process.env.VUE_APP_MAP_API,
+  lang: 'ru_RU',
+}
+
+Vue.use(YmapPlugin, YandexMapsSettings)
 
 new Vue({
   router,
